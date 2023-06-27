cf671b888d36f62c7e493853274294a51a263c99
chandlercarter <cartecl@mail.uc.edu>
2023-06-27 14:10:52 -0400

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,20 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner in = new Scanner(System.in);
+        int birthMonth;                                                             //num birthMonth
 
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        System.out.print("Enter your birth month [1-12]");                          //output "Enter your birth month [1-12]"
+        in.hasNextInt();                                                            //input birthMonth
 
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
+        if (in.hasNextInt()) {                                                      //boolean birthMonth = true
+            birthMonth = in.nextInt();
+            in.nextLine();
+            System.out.println("Your birth month is: N");                           //output "Your birth month is: N"
+        }
+        else {                                                                      // if not birthMonth then
+            System.out.println("You entered an incorrect month value: N");          //output "You entered an incorrect month value: N"
+
         }
     }
 }
